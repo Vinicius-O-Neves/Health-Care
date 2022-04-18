55622a2d5faa0ceb35bf434e842efb09f689589d
Vinicius Neves <vinicius.neeves@outlook.com>
2022-04-17 21:53:36 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import json
 import asyncio
 from imccalculator.data.firebase.FirebaseRepositoryImp import FirebaseRepositoryImp
+from imccalculator.data.firebase.model.PersonInfoModel import PersonInfoModel
 
-asyncio.run(FirebaseRepositoryImp.sendToFirebase("06052022", {'batata': '900g'})) 
+asyncio.run(FirebaseRepositoryImp.sendToFirebase(PersonInfoModel("06052022", {'batata': '400g'}))) 
