9a2653b63428a91996aca0c85608ee3e4edd4bf1
Vinicius Neves <vinicius.neeves@outlook.com>
2022-04-17 21:54:02 -0300

    Inseted PersonInfoModel in sendToFirebase function

--- imccalculator/data/firebase/FirebaseRepositoryImp.py
+++ imccalculator/data/firebase/FirebaseRepositoryImp.py
@@ -2,15 +2,15 @@ import requests
 import json
 import asyncio
 from imccalculator.domain.FirebaseRepository import FirebaseRepository
-from imccalculator.data.firebase.FirebaseDb import FireBaseDb
+from imccalculator.data.firebase.FirebaseDb import FirebaseDb
 
 class FirebaseRepositoryImp(FirebaseRepository):
 
-    async def sendToFirebase(dia, info):
+    async def sendToFirebase(PersonInfoModel):
         try:
             request = requests.post(
-                f'{FireBaseDb().url}/{dia}/.json',
-                data=json.dumps(info)
+                f'{FirebaseDb().url}/{PersonInfoModel.dia}/.json',
+                data = json.dumps(PersonInfoModel.info)
             )
             print(request)
         except Exception as e:
