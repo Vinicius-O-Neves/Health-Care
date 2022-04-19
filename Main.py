885fd544fcb10d58d5ce9be1febfdad22614ebb7
Vinicius Neves <vinicius.neeves@outlook.com>
2022-04-19 14:47:37 -0300

    Changed main folder

--- Main.py
+++
@@ -1,8 +0,0 @@
-import json
-import asyncio
-from imcproject.data.firebase.FirebaseRepositoryImp import FirebaseRepositoryImp
-from imcproject.data.firebase.model.PersonIngestionModel import PersonIngestionModel
-
-asyncio.run(
-    FirebaseRepositoryImp.sendToFirebase(PersonIngestionModel("06052022", {'batata': '300g'}))
-    ) 
--- imcproject/data/firebase/model/PersonIngestionModel.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass, field
-
-@dataclass(order = True)
-class PersonIngestionModel:
-    dia: str
-    info: dict
-   
