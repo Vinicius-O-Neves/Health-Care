350d971e214d604c6211be95a36b0f2f36e4af25
Vinicius Neves <vinicius.neeves@outlook.com>
2022-04-24 17:49:48 -0300

    Update ImcClassifier.py

--- healthcare/ui/imccalculator/controller/classifier/ImcClassifier.py
+++ healthcare/ui/imccalculator/controller/classifier/ImcClassifier.py
@@ -1,20 +1,25 @@
 from WomanClassifier import WomanClassifier
+from ManClassifier import ManClassifier
+from healthcare.resources.Strings import Strings
+
 
 class ImcClassifier:
 
-    def adult(self, imc: int) -> str:
+    @staticmethod
+    def adult(imc: float) -> str:
         interval = {
-            imc < 18.5: "Abaixo do Peso",
-            18.5 <= imc <= 24.9: "Peso Normal",
-            25 <= imc <= 29.9: "Pré-Obesidade",
-            30 <= imc <= 34.9: "Obesidade Grau 1",
-            35 <= imc <= 39.9: "Obesidade Grau 2",
-            imc >= 40: "Obesidade Grau 3" 
+            imc < 18.5: Strings().under_weight,
+            18.5 <= imc <= 24.9: Strings().normal_weight,
+            25 <= imc <= 29.9: Strings().pre_obesity,
+            30 <= imc <= 34.9: Strings().obesity_gdr_1,
+            35 <= imc <= 39.9: Strings().obesity_gdr_2,
+            imc >= 40: Strings().obesity_gdr_3
         }
 
         return interval[True]
 
-    def womanWith10To19(self, age: int, imc: int) -> str:
+    @staticmethod
+    def womanWith10To19(age: int, imc: float) -> str:
         womanClassifier = WomanClassifier(imc)
         interval = {
             age == 10: womanClassifier.womanWith10(),
@@ -28,14 +33,35 @@ class ImcClassifier:
             age == 18: womanClassifier.womanWith18(),
             age == 19: womanClassifier.womanWith19()
         }
-        
+
         pImc = interval[True]
         percentile = {
-            pImc < 5: "Abaixo do Peso",
-            5 <= pImc < 85: "Adequado(a)",
-            pImc >= 85: "Sobrepeso"
+            pImc < 5: Strings().under_weight,
+            5 <= pImc < 85: Strings().appropriate,
+            pImc >= 85: Strings().over_weight
         }
         return percentile[True]
 
-    def manWith10To19(self, age: int):
-        print("homem entre 10 a 19")
+    @staticmethod
+    def manWith10To19(age: int, imc: float) -> str:
+        manClassifier = ManClassifier(imc)
+        interval = {
+            age == 10: manClassifier.manWith10(),
+            age == 11: manClassifier.manWith11(),
+            age == 12: manClassifier.manWith12(),
+            age == 13: manClassifier.manWith13(),
+            age == 14: manClassifier.manWith14(),
+            age == 15: manClassifier.manWith15(),
+            age == 16: manClassifier.manWith16(),
+            age == 17: manClassifier.manWith17(),
+            age == 18: manClassifier.manWith18(),
+            age == 19: manClassifier.manWith19()
+        }
+
+        pImc = interval[True]
+        percentile = {
+            pImc < 5: Strings().under_weight,
+            5 <= pImc < 85: Strings().appropriate,
+            pImc >= 85: Strings().over_weight
+        }
+        return percentile[True]
