c576c650aa9dbf8b8fa100242380a4336b08f8a6
Vinicius Neves <vinicius.neeves@outlook.com>
2022-04-24 17:49:38 -0300

    Update Main.py

--- healthcare/Main.py
+++ healthcare/Main.py
@@ -1,14 +1,13 @@
-import json
 import asyncio
 from datetime import datetime
-from data.firebase.FirebaseRepositoryImp import FirebaseRepositoryImp
+from data.firebase.repository.FirebaseRepositoryImp import FirebaseRepositoryImp
 from data.firebase.model.PersonIngestionModel import PersonIngestionModel
 
 asyncio.run(
     FirebaseRepositoryImp.sendToFirebase(PersonIngestionModel(
-        4421150987652, 
+        4421150987652,
         datetime.today().strftime("%Y-%m-%d").replace('-', ''),
-        {'pao': '300g', 'feijao': '70g'}
+        {'pao': '300g', 'aaaaa': '70g'}
         )
     )
-) 
+)
