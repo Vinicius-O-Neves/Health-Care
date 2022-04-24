7e4ba50757ebf42552735e3174e8dc8289dcb88a
Vinicius Neves <vinicius.neeves@outlook.com>
2022-04-24 17:49:43 -0300

    Update Strings.py

---
+++ healthcare/resources/Strings.py
@@ -0,0 +1,15 @@
+class Strings:
+
+    def __init__(self):
+        self.male = "masculino"
+        self.female = "feminino"
+
+        self.under_weight = "Abaixo do Peso"
+        self.normal_weight = "Peso Normal"
+        self.pre_obesity = "Pré-Obesidade"
+        self.obesity_gdr_1 = "Obesidade Grau 1"
+        self.obesity_gdr_2 = "Obesidade Grau 2"
+        self.obesity_gdr_3 = "Obesidade Grau 3"
+
+        self.appropriate = "Adequado(a)"
+        self.over_weight = "Sobrepeso"
