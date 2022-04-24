d68cd83111ff2a6b3c29fe3387503750534c5825
Vinicius Neves <vinicius.neeves@outlook.com>
2022-04-24 17:49:34 -0300

    Update ImcCalculator.py

--- healthcare/ui/imccalculator/controller/classifier/ImcCalculator.py
+++ healthcare/ui/imccalculator/controller/classifier/ImcCalculator.py
@@ -1,8 +1,10 @@
 from ImcClassifier import ImcClassifier
+from healthcare.resources.Strings import Strings
+
 
 class ImcCalculator:
 
-    def calculate(self, weight: int, height: float) -> int:
+    def calculate(self, weight: int, height: float) -> float:
         self.imc = weight / (height ** 2)
         return round(self.imc, 2)
 
@@ -11,11 +13,12 @@ class ImcCalculator:
 
         if age >= 20:
             return ImcClassifier().adult(self.imc)
-        elif 20 > age >= 10 and self.gender == "masculino":
-            pass
-        elif 20 > age >= 10 and self.gender == "feminino":
+        elif 20 > age >= 10 and self.gender == Strings().male:
+            return ImcClassifier().manWith10To19(age, self.imc)
+        elif 20 > age >= 10 and self.gender == Strings().female:
             return ImcClassifier().womanWith10To19(age, self.imc)
-            
+
+
 imc = ImcCalculator()
-print(imc.calculate(92, 1.83))
-print(imc.analyze(49, "masculino"))
+print(imc.calculate(62, 1.83))
+print(imc.analyze(19, "masculino"))
