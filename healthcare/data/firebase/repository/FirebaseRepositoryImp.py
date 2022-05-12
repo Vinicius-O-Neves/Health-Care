71f00484dff0a556365e4e3571ae350e86de388d
Vinicius Neves <vinicius.neeves@outlook.com>
2022-05-12 12:50:50 -0300

    This class dont extends from an interface now

--- healthcare/data/firebase/repository/FirebaseRepositoryImp.py
+++ healthcare/data/firebase/repository/FirebaseRepositoryImp.py
@@ -1,14 +1,13 @@
 import requests
 import json
 from typing import Type
-from healthcare.domain.FirebaseRepository import FirebaseRepository
 from healthcare.data.firebase.FirebaseDb import FirebaseDb
 from healthcare.data.firebase.model.PersonIngestionModel import PersonIngestionModel
 
 '''Classe que contem todas funções para acesso e leitura no Firebase'''
 
 
-class FirebaseRepositoryImp(FirebaseRepository):
+class FirebaseRepositoryImp:
 
     @staticmethod
     async def getFromFirebase(ingestion=Type[PersonIngestionModel]):
