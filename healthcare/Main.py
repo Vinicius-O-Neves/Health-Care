74b649850ca1272b4507697f175225af3bad5419
Vinicius Neves <vinicius.neeves@outlook.com>
2022-05-17 18:02:41 -0300

    Update Main.py

--- healthcare/Main.py
+++ healthcare/Main.py
@@ -3,11 +3,16 @@ from datetime import datetime
 from healthcare.data.firebase.repository.FirebaseRepositoryImp import FirebaseRepositoryImp
 from healthcare.data.firebase.model.PersonIngestionModel import PersonIngestionModel
 
+FirebaseRepositoryImp.sendIngestionToFirebase(PersonIngestionModel(
+    "4cd5c7c0-7918-40a8-a551-aed42b817524",
+    datetime.today().strftime("%Y-%m-%d").replace('-', ''),
+    {'tatata': '300g', 'aaa': '70g'}
+    )
+)
+
 asyncio.run(
-    FirebaseRepositoryImp.sendToFirebase(PersonIngestionModel(
-        4421150987652,
-        datetime.today().strftime("%Y-%m-%d").replace('-', ''),
-        {'pao': '300g', 'aaaaa': '70g'}
-        )
+    FirebaseRepositoryImp.getIngestionFromFirebase(
+        "cfa51380-7475-4449-af7e-50ba71a3b11b",
+        datetime.today().strftime("%Y-%m-%d").replace('-', '')
     )
 )
