a3cae3085a82aa8813fabb5b1431e85e538f975f
Vinicius Neves <vinicius.neeves@outlook.com>
2022-05-17 23:52:07 -0300

    Added return in functions

--- healthcare/data/firebase/repository/FirebaseRepositoryImp.py
+++ healthcare/data/firebase/repository/FirebaseRepositoryImp.py
@@ -1,31 +1,43 @@
 import requests
 import json
-from typing import Type
+from typing import Type, List
+
+from requests import Response
+
 from healthcare.data.firebase.FirebaseDb import FirebaseDb
 from healthcare.data.firebase.model.PersonIngestionModel import PersonIngestionModel
 
 '''Classe que contem todas funções para acesso e leitura no Firebase'''
+
+
 class FirebaseRepositoryImp:
 
     @staticmethod
-    async def getIngestionFromFirebase(id: str, day: str):
+    async def getIngestionFromFirebase(id: str, day: str) -> list[str] | str:
         try:
             request = requests.get(
                 f'{FirebaseDb().BASE_URL}/{id}/{day}/.json'
             )
-            print(request.text)
+            ingestionList = [
+                str(request.json()[key])
+                    .replace("{", "")
+                    .replace("}", "")
+                for key in request.json()
+            ]
+
+            return ingestionList
         except Exception as e:
-            print("Erro ao tentar puxar do Firebase")
             print(str(e))
+            return "Erro ao tentar puxar do Firebase"
 
     @staticmethod
-    def sendIngestionToFirebase(ingestion=Type[PersonIngestionModel]):
+    def sendIngestionToFirebase(ingestion=Type[PersonIngestionModel]) -> Response | str:
         try:
             request = requests.post(
                 f'{FirebaseDb().BASE_URL}/{ingestion.id}/{ingestion.day}/.json',
                 data=json.dumps(ingestion.info)
             )
-            print(request.text)
+            return request
         except Exception as e:
-            print("Erro ao tentar enviar o Firebase")
             print(str(e))
+            return "Erro ao tentar enviar o Firebase"
