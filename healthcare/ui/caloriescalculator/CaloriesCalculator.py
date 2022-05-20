e8d087d75f546bb8bfa3deafb88db3de6045dd2f
Vinicius Neves <vinicius.neeves@outlook.com>
2022-05-20 14:54:50 -0300

    Nothing Changed since last commit from lucas

--- healthcare/ui/caloriescalculator/CaloriesCalculator.py
+++ healthcare/ui/caloriescalculator/CaloriesCalculator.py
@@ -1,14 +1,13 @@
 import asyncio
 import re
 from datetime import datetime
-from healthcare.data.firebase.model.PersonIngestionModel import *
 from healthcare.data.firebase.repository.FirebaseRepositoryImp import FirebaseRepositoryImp
 
 
 class CaloriesCalculator:
 
+    @staticmethod
     def totalConsumedCalories(id: str) -> tuple[float | int, float]:
-
         ingestionList = asyncio.run(FirebaseRepositoryImp.getIngestionFromFirebase(
             id,
             datetime.today().strftime("%Y-%m-%d").replace('-', '')
@@ -16,14 +15,16 @@ class CaloriesCalculator:
         total = sum([float(item) for item in re.findall("\d+", str(ingestionList))])
         dailyLimit = 3000
         consumedCalories = dailyLimit - total
+
         return total, consumedCalories
 
-    def listOfIngestion(id: str) -> list[PersonIngestionModel]:
-
+    @staticmethod
+    def listOfIngestion(id: str) -> list[str]:
         response = asyncio.run(FirebaseRepositoryImp.getIngestionFromFirebase(
             id,
             datetime.today().strftime("%Y-%m-%d").replace('-', '')
         ))
+
         return [str(items).replace("'", '') for items in response]
 
 print(CaloriesCalculator.totalConsumedCalories("4cd5c7c0-7918-40a8-a551-aed42b817524"))
