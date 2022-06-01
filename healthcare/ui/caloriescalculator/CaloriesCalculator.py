01b782e9a35c8cecb03101b9ec25a69fa06ab7f8
Vinicius Neves <vinicius.neeves@outlook.com>
2022-06-01 13:39:16 -0300

    Funções criadas para calcular o quanto a pessoa precisa comer

--- healthcare/ui/caloriescalculator/CaloriesCalculator.py
+++ healthcare/ui/caloriescalculator/CaloriesCalculator.py
@@ -27,5 +27,27 @@ class CaloriesCalculator:
 
         return [str(items).replace("'", '') for items in response]
 
-print(CaloriesCalculator.totalConsumedCalories("4cd5c7c0-7918-40a8-a551-aed42b817524"))
-print(CaloriesCalculator.listOfIngestion("4cd5c7c0-7918-40a8-a551-aed42b817524"))
+    @staticmethod
+    def manNeedsCalories(age: int, weight: float) -> float:
+        interval = {
+            0 >= age < 3: ((59.512 * weight) - 30.4),
+            3 >= age < 10: ((22.706 * weight) + 504.3),
+            10 >= age < 18: ((17.686 * weight) + 658.2),
+            18 >= age < 30: ((15.057 * weight) + 692.2),
+            30 >= age < 60: ((11.472 * weight) + 873.1),
+            age >= 60: ((11.711 * weight) + 587.7)
+        }
+        return interval[True]
+
+    @staticmethod
+    def womanNeedCalories(age: int, weight: float) -> float:
+        interval = {
+            0 >= age < 3: ((58.317 * weight) - 31.1),
+            3 >= age < 10: ((20.315 * weight) + 485.9),
+            10 >= age < 18: ((13.384 * weight) + 692.6),
+            18 >= age < 30: ((14.818 * weight) + 486.6),
+            30 >= age < 60: ((8.126 * weight) + 845.6),
+            age >= 60: ((9.082 * weight) + 658.5)
+
+        }
+        return interval[True]
