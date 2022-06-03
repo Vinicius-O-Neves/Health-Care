ebe3e0d011c937c5fbe1c67ec17435c3601c6fdb
Vinicius Neves <vinicius.neeves@outlook.com>
2022-06-02 22:55:01 -0300

    Update Colors.py

---
+++ healthcare/resources/Colors.py
@@ -0,0 +1,8 @@
+class Colors:
+
+    def __init__(self):
+        self.backgroundColor = "#F2CCCC"
+        self.titleColor = "#FF2113"
+        self.entryColor = "#C4C4C4"
+        self.buttonColor = "#FAFAFA"
+        self.purpleColor = "#B72CA9"
